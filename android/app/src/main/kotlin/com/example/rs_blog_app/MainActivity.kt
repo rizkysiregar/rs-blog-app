aa42493e593d528c0bbb327a46f0805a600081e4
Rizky Siregar <regarrezky@gmail.com>
2024-05-16 17:12:14 +0700

    first init

---
+++ android/app/src/main/kotlin/com/example/rs_blog_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rs_blog_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/theme/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:rs_blog_app/core/theme/app_pallete.dart';
+
+class AppTheme {
+  static final darkThemeMode = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: AppPallete.backgroundColor
+  );
+}
